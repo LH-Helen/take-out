25e195ac932e71a4fca3d4ab36a9ac9c752528e7
刘贺 <lh_helen2019@163.com>
2024-10-14 11:40:42 +0800

    菜品分页查询业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,8 +1,11 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -11,6 +14,7 @@ import org.apache.ibatis.annotations.Select;
 public interface DishMapper {
     /**
      * 根据分类id查询计数
+     *
      * @param id
      * @return
      */
@@ -19,8 +23,17 @@ public interface DishMapper {
 
     /**
      * 插入菜品
+     *
      * @param dish
      */
     @AutoFill(value = OperationType.INSERT)
     void insert(Dish dish);
+
+    /**
+     * 菜品分页查询
+     *
+     * @param dishPageQueryDTO
+     * @return
+     */
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
