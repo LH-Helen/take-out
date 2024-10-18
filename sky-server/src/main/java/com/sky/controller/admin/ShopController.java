0d6e7b7c9347fd2819d3941c8b351bb399209144
刘贺 <lh_helen2019@163.com>
2024-10-18 22:26:12 +0800

    微信小程序用户登录功能业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -3,7 +3,6 @@ package com.sky.controller.admin;
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.models.auth.In;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
