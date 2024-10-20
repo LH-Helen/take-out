82316da4705115327e10abc71285f8744d4a05f6
刘贺 <lh_helen2019@163.com>
2024-10-20 21:26:11 +0800

    缓存菜品业务功能代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,9 +11,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * 菜品管理
@@ -27,6 +29,19 @@ public class DishController {
     @Autowired
     private DishService dishService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+    /**
+     * 清理缓存数据
+     * @param pattern
+     */
+    private void cleanCache(String pattern){
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
+    }
+
     /**
      * 新增菜品
      *
@@ -38,6 +53,10 @@ public class DishController {
     public Result save(@RequestBody DishDTO dishDTO) {
         log.info("新增菜品：{}", dishDTO);
         dishService.saveWithFlavor(dishDTO);
+
+        // 清理缓存数据
+        cleanCache("dish_" + dishDTO.getCategoryId());
+
         return Result.success();
     }
 
@@ -66,6 +85,10 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids) {
         log.info("根据id批量删除菜品：{}", ids);
         dishService.deleteBatch(ids);
+
+        // 缓存全删了
+        cleanCache("dish_*");
+
         return Result.success();
     }
 
@@ -94,6 +117,10 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO) {
         log.info("修改菜品, {}", dishDTO);
         dishService.updateWithFlavor(dishDTO);
+
+        // 缓存全删了
+        cleanCache("dish_*");
+
         return Result.success();
     }
 
@@ -109,6 +136,10 @@ public class DishController {
     public Result startOrStop(@PathVariable Integer status, Long id) {
         log.info("启用、禁用分类, {}", status);
         dishService.startOrStop(status, id);
+
+        // 缓存全删了
+        cleanCache("dish_*");
+
         return Result.success();
     }
 
